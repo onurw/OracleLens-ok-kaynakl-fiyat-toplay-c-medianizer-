52a026d6be854bd348915b75d0a4f2f27d249897
onurw <80415348+onurw@users.noreply.github.com>
2025-09-27 17:42:14 +0300

    Create hardhat.config.js

---
+++ hardhat.config.js
@@ -0,0 +1,11 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+module.exports = {
+  solidity: "0.8.24",
+  networks: {
+    hardhat: {}
+    // örnek:
+    // sepolia: { url: process.env.SEPOLIA_RPC, accounts: [process.env.PRIVATE_KEY] }
+  }
+};
